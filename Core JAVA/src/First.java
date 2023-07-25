72ed7001caceeae04114807e6c7f3ccf4804d78d
jay-java <jayprajapati.tops@gmail.com>
2023-07-25 12:55:30 +0530

    first lecture

---
+++ Core JAVA/src/First.java
@@ -0,0 +1,14 @@
+
+class another{
+	public static void main(String[] args) {
+		System.out.println("hello another class");
+	}
+}
+public class First {
+	public static void main(String[] args) {
+		System.out.println("hello main function");
+	}
+	public static void main(Integer[] args) {
+		System.out.println("another main");
+	}
+}
---
+++ Core JAVA/src/aaa.java
@@ -0,0 +1,9 @@
+
+public class aaa {
+	public static void main(String[] args) {
+		System.out.println("hello main function");
+	}
+	public static void main(Integer[] args) {
+		System.out.println("another main");
+	}
+}
